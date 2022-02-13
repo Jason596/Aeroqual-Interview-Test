dcdf6712f93cdb2612e7301dd33612648df9ae43
Jason Wu <Jason.Wu@middleware.co.nz>
2022-02-13 22:27:22 +1300

    annotation for the model

--- AeroqualCloudTest-master/ApiTest/Models/People.cs
+++ AeroqualCloudTest-master/ApiTest/Models/People.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace ApiTest.Models
 {
     public class People
     {
+        [Required]
         [JsonPropertyName("People")]
         public List<Person> ListOfPeoplePersons { get; set; }
     }
--- AeroqualCloudTest-master/ApiTest/Models/Person.cs
+++ AeroqualCloudTest-master/ApiTest/Models/Person.cs
@@ -1,16 +1,20 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace ApiTest.Models
 {
     public class Person
     {
+        [Required]
         [JsonPropertyName("Id")]
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
+        [Required]
         [JsonPropertyName("Name")]
         public string Name { get; set; }
 
+        [Required]
         [JsonPropertyName("Age")]
-        public int Age { get; set; }
+        public int? Age { get; set; }
     }
 }
