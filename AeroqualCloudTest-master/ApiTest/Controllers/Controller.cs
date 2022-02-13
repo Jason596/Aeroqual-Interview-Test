ee58ba3bf80017dfbd11f1063af9c2164dd4b8c8
Jason Wu <Jason.Wu@middleware.co.nz>
2022-02-13 18:52:52 +1300

    update

--- AeroqualCloudTest-master/ApiTest/Controllers/Controller.cs
+++ AeroqualCloudTest-master/ApiTest/Controllers/Controller.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using ApiTest.Interfaces;
 using ApiTest.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiTest.Controllers
